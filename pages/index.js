d9c963c38f06b10428daae9cffb2a24f8cccc662
Diego Trevisan <960548@lojasrenner.com.br>
2023-09-05 08:06:26 -0300

    added some images

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import styles from '../styles/Home.module.css'
 import Head from 'next/head' //head meta-tags
+import Image from 'next/image' //image optimization
+import cityImage from '../public/assets/images/city.jpg' //image
 export default function Home() {
   return (
   <>
@@ -7,7 +9,10 @@ export default function Home() {
     <title>Home</title>
     <meta name="keywords" content='Roupas, Calçados, Meias'></meta>
   </Head>
+  <div>
     <h1 className={styles.title}>Oi</h1>
+    <Image src={cityImage} width="300px" height="400px" alt="Cidade a noite"/>
+  </div>
   </>
   )
 }
